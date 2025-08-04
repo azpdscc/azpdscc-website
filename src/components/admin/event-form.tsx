54d21337bd9ea0863e218366c2eb981e71dfcd68
Kulraj Singh <kulraj@azpdscc.org>
2025-08-04 06:09:04 +0000

    the calendar click a date is misaligned with teh time feld and is ll red

--- src/components/admin/event-form.tsx
+++ src/components/admin/event-form.tsx
@@ -133,7 +133,7 @@ export function EventForm({ type, event, action }: EventFormProps) {
               <Popover>
                 <PopoverTrigger asChild>
                   <FormControl>
-                    <Button variant="outline" className={cn("w-full justify-start text-left font-normal", !field.value && "text-muted-foreground")}>
+                    <Button variant={"outline"} className={cn("w-full justify-start text-left font-normal", !field.value && "text-muted-foreground")}>
                       <CalendarIcon className="mr-2 h-4 w-4" />
                       {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
                     </Button>
