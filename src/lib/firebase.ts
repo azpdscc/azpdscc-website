a7c696856364649b6c40cee7a7fe3f487505cc14
Kulraj Singh <kulraj@azpdscc.org>
2025-08-06 19:24:41 +0000

    git hub just told me that there is a google api key that is exposed in s

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   "projectId": "azpdscc-hub",
   "appId": "1:594549470017:web:4c36f8e414cc3dcbc067c0",
   "storageBucket": "azpdscc-hub.firebasestorage.app",
-  "apiKey": "AIzaSyDwpzJ1tR_2ljCm-knQwaghPuTjgwbvY2I",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "azpdscc-hub.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "594549470017"
