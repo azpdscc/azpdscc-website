714b437ce3496c68c67f55797862abe3552369a0
Kulraj Singh <kulraj@azpdscc.org>
2025-09-02 02:52:56 +0000

    I don't want you to touch anything else don't begin anything else other

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,11 +5,16 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://pdscc.org';
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+            '/admin/', 
+            '/verify-ticket',
+        ],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
